08f61b33fb7af12bd8ce45ed9b7db717d56f45bf
Hectorrreus <hectorrreus@gmail.com>
2022-03-31 18:56:10 +0200

    Entrega ejercicio spark-kafka

--- spark/entrega_Spark-Kafka/spark_HVC/escribir_pedidos_stifencurry.py
+++ spark/entrega_Spark-Kafka/spark_HVC/escribir_pedidos_stifencurry.py
@@ -1,3 +1,4 @@
+#Importo librerías
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, FloatType
